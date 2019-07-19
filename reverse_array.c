8286319cda325a61d86a711b5e8a7c01b3793b81
Vaibhav Bahl <vaibhav171965.cse@chitkara.edu.in>
2019-07-19 09:38:18 +0530

    Reverse Array

---
+++ reverse_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	int size=argc-1;
+	int arr[size];
+	for(int i=0;i<size;i++)
+	{
+		arr[i] = atoi(argv[i+1]);
+	}
+	for(int i=0;i<(size);i++)
+	{
+		printf("%d\n",arr[i]);
+	}
+	printf("\n\n\n");
+	int temp,size1=size-1;
+	for(int i=0;i<((size-1)/2);i++)
+	{
+		temp=arr[size1];
+		arr[size1]=arr[i];
+		arr[i]=temp;
+		size1--;
+	}
+	for(int i=0;i<(size);i++)
+	{
+		printf("%d\n",arr[i]);
+	}
+	return 0;
+}
