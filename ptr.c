be070272e6bfb164ace0d1dde678b18e2ee55529
Vaibhav Bahl <vaibhav171965.cse@chitkara.edu.in>
2019-07-20 10:53:50 +0530

    UP

--- ptr.c
+++ ptr.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char const *argv[])
 {
-	int a[] = {1,2,3,4};
-	int *b = a;
-	for(int i=0;i<4;i++)
+	int size=argc-1;
+	int arr[size];
+	for(int i=0;i<size;i++)
 	{
-		printf("%u\n",*(b+i));
+		arr[i] = atoi(argv[i+1]);
+	}
+	int *ptr=arr;
+	int temp,size1=size-1;
+	for(int i=0;i<((size-1)/2);i++)
+	{
+		temp=ptr[size1];
+		ptr[size1]=ptr[i];
+		ptr[i]=temp;
+		size1--;
+	}
+	for(int i=0;i<(size);i++)
+	{
+		printf("%d\n",arr[i]);
 	}
 	return 0;
 }
